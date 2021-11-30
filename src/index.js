500c7f260579fa425a997f019c9bd72a9deb86b4
Himanshu <hy287719@gmail.com>
2021-11-30 18:07:16 +0530

    new features are added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+let allRestaurants = [];
 const app = Vue.createApp({
     data() {
         return {
@@ -10,13 +11,37 @@ const app = Vue.createApp({
     created() {
         fetch("https://mocki.io/v1/3fb1488d-bbdb-4ddd-9a03-a0d2efc98597")
             .then(res => res.json())
-            .then(data => this.allRestaurants = data)
+            .then(data => {
+                this.allRestaurants = data;
+                console.log("data ", data);
+                allRestaurants = data;
+            })
             .catch(err => console.log("Error ", err));
     },
     methods: {
         processName(name) {
             return name.charAt(0).toUpperCase() + name.slice(1);
+        },
+        mergeCategroy(category) {
+            return category.split(' ').join('');
+        },
+        showCategoryItems(category) {
+            const allItems = document.getElementsByClassName('hide_'+ this.mergeCategroy(category));
+            console.log('allItems ',allItems.length);
+            for(let i = 0; i<allItems.length; i++) {
+                allItems[i].style.display = "block";
+            }
+            document.getElementById('show_' + this.mergeCategroy(category)).style.display = "none";
         }
     }
 });
-app.mount('#app');
+const appInstance = app.mount('#app');
+// allRestaurants = appInstance.allRestaurants;
+// console.log('allRestaurants ',allRestaurants);
+// appInstance.$watch('allRestaurants', function (newValue, oldValue) {
+//     const elementId = 'hide_' + appInstance.mergeCategroy(newValue[0].category);
+//     console.log(elementId);
+//     setTimeout(()=>{
+//         console.log(document.getElementById(elementId));
+//     },2000);
+// });
