0637467b97a05d0d7edf84bdefa3cbfaa5c78fd0
Himanshu <hy287719@gmail.com>
2021-12-01 21:45:44 +0530

    Added some extra feature

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const app = Vue.createApp({
         return {
             message: "Hello",
             allRestaurants: [],
+            swiggyRestaurants: [],
+            seeAll: false,
             loading: true,
-            error: null
+            error: null,
+            imagesUrl: ["https://images.unsplash.com/photo-1484723091739-30a097e8f929?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1521305916504-4a1121188589?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1511690743698-d9d85f2fbf38?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1429554513019-6c61c19ffb7e?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1496412705862-e0088f16f791?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1432139509613-5c4255815697?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1484980972926-edee96e0960d?auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?auto=format&fit=crop&w=500&q=60"]
         }
     },
     created() {
@@ -13,8 +16,7 @@ const app = Vue.createApp({
             .then(res => res.json())
             .then(data => {
                 this.allRestaurants = data;
-                console.log("data ", data);
-                allRestaurants = data;
+                this.getAllSwiggyRestaurant(data);
             })
             .catch(err => console.log("Error ", err));
     },
@@ -22,20 +24,65 @@ const app = Vue.createApp({
         processName(name) {
             return name.charAt(0).toUpperCase() + name.slice(1);
         },
-        mergeCategroy(category) {
+        mergeCategory(category) {
             return category.split(' ').join('');
         },
         showCategoryItems(category) {
-            const allItems = document.getElementsByClassName('hide_'+ this.mergeCategroy(category));
+            const allItems = document.getElementsByClassName('hide_'+ this.mergeCategory(category));
             console.log('allItems ',allItems.length);
             for(let i = 0; i<allItems.length; i++) {
                 allItems[i].style.display = "block";
             }
-            document.getElementById('show_' + this.mergeCategroy(category)).style.display = "none";
+            document.getElementById('show_' + this.mergeCategory(category)).style.display = "none";
+            document.getElementById('show_' + this.mergeCategory(category)).parentNode.classList.add('hoverEffect');
+        },
+        getAllSwiggyRestaurant(restaurants) {
+            let allSwiggyRestaurant = [];
+            restaurants.forEach((restaurantList)=>{
+                const temp = restaurantList.restaurantList;
+                console.log("restaurant list ", temp);
+                temp.forEach((restaurant)=>{
+                    if(restaurant.isExlusive) {
+                        allSwiggyRestaurant.push(restaurant);
+                    }
+                })
+            })
+            this.swiggyRestaurants = allSwiggyRestaurant;
+            console.log(allSwiggyRestaurant);
         }
     }
 });
 const appInstance = app.mount('#app');
+const sections = document.getElementsByClassName("brandContainer");
+let current = "";
+document.addEventListener('scroll',()=>{
+    for(let i = 0; i<sections.length; i++) {
+        const section = sections[i];
+        const top = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(scrollY >= (top - sectionHeight/3)) {
+            current = (section.getAttribute('id'));
+        }
+    }
+    setActiveClass(current);
+})
+
+function setActiveClass(current) {
+    let a = document.getElementsByClassName('restaurantType');
+    if(current === '') {
+        console.log('a[0] ',a[0]);
+    } else {
+        console.log('current ', current, ' a ', a);
+        for(let i = 0; i<a.length; i++) {
+            a[i].classList.remove("active");
+            if(a[i].classList.contains(current)) {
+                console.log("Yes");
+                a[i].classList.add("active");
+            }
+        }
+    }
+}
+
 // allRestaurants = appInstance.allRestaurants;
 // console.log('allRestaurants ',allRestaurants);
 // appInstance.$watch('allRestaurants', function (newValue, oldValue) {
